81a8620c0c252f4751b6b6c12b23831e0d1b348c
Lior Rachmani <liorachmani@gmail.com>
2025-04-04 13:25:24 +0300

    queries

--- src/constants/QueryKeys.ts
+++ src/constants/QueryKeys.ts
@@ -4,4 +4,8 @@ export const QUERY_KEYS = {
   POSTS_BY_USER: (userId: string) => `posts-${userId}`,
   COMMENTS_BY_POST: (postId: string) => `comments-${postId}`,
   USER_BY_ID: (id: string) => `user-${id}`,
+  CLASSES: "classes",
+  CLASS_BY_ID: (id: string) => `class-${id}`,
+  STUDENTS: "students",
+  STUDENTS_BY_CLASS_ID: (id: string) => `students-${id}`,
 } as const;

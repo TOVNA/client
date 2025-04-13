3886894f646c35f8eb0cac0a393cc80fbc471618
Lior Rachmani <liorachmani@gmail.com>
2025-04-13 12:57:49 +0300

    no class message

---
+++ src/components/FullScreenMessage/FullScreenMessage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./FullScreenMessage.module.css";
+
+interface FullScreenMessageProps {
+  title: string;
+  message: string;
+}
+
+const FullScreenMessage: React.FC<FullScreenMessageProps> = ({
+  title,
+  message,
+}) => {
+  return (
+    <div className={styles.fullScreenMessage}>
+      <div>{title}</div>
+      <div>{message}</div>
+    </div>
+  );
+};
+
+export default FullScreenMessage;

5a477b44d7ee629f4e6f25ec3b57e86c39ae50ef
Lior Rachmani <liorachmani@gmail.com>
2025-05-28 19:31:54 +0300

    Question comp

---
+++ src/components/Question/Question.tsx
@@ -0,0 +1,45 @@
+import { Question as IQuestion } from "../../types/entities/question";
+import styles from "./Question.module.css";
+
+interface QuestionProps {
+  question: IQuestion;
+  isEditable: boolean;
+  answer: string;
+  onAnswerChange: (newAnswer: string) => void;
+  error?: string;
+}
+
+export const Question: React.FC<QuestionProps> = ({
+  question,
+  isEditable,
+  answer,
+  onAnswerChange,
+  error,
+}) => {
+  const handleAnswerChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onAnswerChange(e.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>{question.text}</h3>
+      <p>{question.description}</p>
+      <div>
+        <label>תשובה:</label>
+        {isEditable ? (
+          <textarea
+            value={answer}
+            onChange={handleAnswerChange}
+            className={`${styles.textarea} ${
+              error ? styles.textareaError : ""
+            }`}
+            rows={4}
+          />
+        ) : (
+          <p>{answer}</p>
+        )}
+        {error && <p className={styles.errorMessage}>{error}</p>}
+      </div>
+    </div>
+  );
+};

f6278764440d77dc520a155c8511af9a4ca7d67c
Lior Rachmani <liorachmani@gmail.com>
2025-08-09 23:43:12 +0300

    navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,31 @@
+
 import styles from "./Navbar.module.css";
 import NavbarButton from "./NavbarButton/NavbarButton";
 import LogoutIcon from "../../assets/logout.svg";
+import UserIcon from "../../assets/user.svg";
 import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useCallback } from "react";
 
 const Navbar = () => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
+  const navigate = useNavigate();
+
+  const openUserProfile = useCallback(() => {
+    navigate(`/teacher/${user?._id}`);
+  }, [navigate, user?._id]);
+
+  const goToMainPage = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
 
   return (
     <div className={styles.navbar}>
-      <span className={styles.navbarTitle}>תובנ"ה</span>
-      <NavbarButton iconSrc={LogoutIcon} onClick={logout} name="התנתק" />
+      <span className={styles.navbarTitle} onClick={goToMainPage}>תובנ"ה</span>
+      <div className={styles.navbarButtonContainer}>
+        <NavbarButton iconSrc={UserIcon} onClick={openUserProfile} />
+        <NavbarButton iconSrc={LogoutIcon} onClick={logout} name="התנתק" />
+      </div>
     </div>
   );
 };
